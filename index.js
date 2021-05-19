afce99caf8a6b0f3ba9181cda6b31471ccf5737f
dumbasPL <29180158+dumbasPL@users.noreply.github.com>
2021-05-19 17:13:12 +0200

    why am i even doing this

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ var geoip = require('geoip-lite');
 
 const port = 51337;
 
-app.get('/benawad_will_kill_me/why_am_i_doing_this.png', (req, res) => {
+app.get('/avatars/benawad_will_kill_me/why_am_i_doing_this.png', (req, res) => {
   const encoder = new GIFEncoder(100, 100);
   encoder.createReadStream().pipe(res);
-  
+
   encoder.start();
   encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
   encoder.setDelay(1000);  // frame delay in ms
   encoder.setQuality(10); // image quality. 10 is default.
-  
+
   // use node-canvas
   const canvas = createCanvas(100, 100);
   const ctx = canvas.getContext('2d');
 
-  let ip = res.header['X-Forwarded-For'] ?? req.ip;
-  
+  let ip = req.headers['x-forwarded-for'] ?? req.ip;
+  //console.log(req);
   ctx.font = '14px Arial';
   ctx.fillStyle = '#550000';
   ctx.fillRect(0, 0, 100, 100);
   ctx.fillStyle = '#FFFFFF';
   ctx.fillText("your ip is:", 20, 40, 90);
-  ctx.fillText(req.ip, 5, 65, 90);
+  ctx.fillText(ip, 5, 65, 90);
   encoder.addFrame(ctx);
-  
+
   var geo = geoip.lookup(ip);
 
   if (geo) {
@@ -38,15 +38,10 @@ app.get('/benawad_will_kill_me/why_am_i_doing_this.png', (req, res) => {
     ctx.fillStyle = '#FFFFFF';
     ctx.fillText("your are from:", 5, 40, 90);
     ctx.fillText(geo.country, 40, 65, 90);
-  } 
+  }
 
   encoder.addFrame(ctx);
-  
-  // // blue rectangle
-  // ctx.fillStyle = '#0000ff';
-  // ctx.fillRect(0, 0, 320, 240);
-  // encoder.addFrame(ctx);
-  
+
   encoder.finish();
 
   // re
